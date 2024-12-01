054668cad721b421591811dabd8115c45cee6087
Kleber Tiko <klebertiko@gmail.com>
2024-11-30 23:54:00 -0300

    onetwo code

--- hello_onetwo.py
+++ hello_onetwo.py
@@ -6,20 +6,5 @@ import os
 # Confirm the currently installed OneTwo version.
 print(f'Running OneTwo v{ot.__version__}.')
 
-if not 'OPENAI_API_KEY' not in os.environ:
-  raise ValueError(
-      'The api key must be specified either here or in the environment.')
-
-model_name = 'gpt-4o-mini'  # Specify the model of your choice.
-
-# Create and register a connection to the OpenAI backend.
-backend = openai_api.OpenAIAPI(
-    api_key=os.environ["OPENAI_API_KEY"],
-    model_name=model_name,
-    temperature=0.0,
-    max_retries=12,
-    cache_filename=get_cache_path(model_name, temperature=0.0),
-)
-backends[model_name] = backend
 backend.register()
-print('OpenAI API backend registered.')
+print('Backend registered.')
--- hello_onetwo_gemini.py
+++ hello_onetwo_gemini.py
@@ -1,6 +1,6 @@
 from onetwo import ot
 from onetwo.builtins import llm
-from onetwo.backends import openai_api
+from onetwo.backends import gemini_api
 import os
 
 # Confirm the currently installed OneTwo version.
@@ -10,16 +10,15 @@ if not 'GEMINI_API_KEY' not in os.environ:
   raise ValueError(
       'The api key must be specified either here or in the environment.')
 
-model_name = 'gemini-pro'  # Specify the model of your choice.
+model_name = 'GEMINI_PRO'  # Specify the model of your choice.
 
-# Create and register a connection to the OpenAI backend.
-backend = openai_api.OpenAIAPI(
-    api_key=os.environ["OPENAI_API_KEY"],
-    model_name=model_name,
+# Create and register a connection to the Gemini backend.
+backend = gemini_api.GeminiAPI(
+    api_key=os.environ["GEMINI_API_KEY"],
     temperature=0.0,
     max_retries=12,
     cache_filename=get_cache_path(model_name, temperature=0.0),
 )
 backends[model_name] = backend
 backend.register()
-print('OpenAI API backend registered.')
+print('Gemini API backend registered.')
